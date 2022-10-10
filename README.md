a3f878c156584651353c1ac568565622671b0235
One-under-all <108278524+One-under-all@users.noreply.github.com>
2022-10-10 13:14:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# js-basics
+ 1st tutorial
---
+++ index.js
@@ -0,0 +1,5 @@
+let name = 'Allis'; // String Literal
+let age = 18; // Number Literal
+let isApproved = true; // Boolean Litteral
+let firstName;
+let selectedColor = null;
